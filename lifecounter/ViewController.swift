8d1555f28bef2f77e6aea7b76cead03b3580d4fc
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-04-21 19:17:57 -0700

    implemented counter game

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -12,8 +12,75 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        losingLabel.isHidden = true
     }
+    
+    var playerOneLifeVal = 20
+    var playerTwoLifeVal = 20
+    
+    @IBOutlet weak var playerOneLifeLabel: UILabel!
+    
+    @IBOutlet weak var playerTwoLifeLabel: UILabel!
+    
+    @IBOutlet weak var losingLabel: UILabel!
 
-
+    @IBAction func playerOnePlusTapped(_ sender: UIButton) {
+        playerOneLifeVal += 1
+        updateLifeLabels()
+    }
+    
+    @IBAction func playerOnePlusFiveTapped(_ sender: UIButton) {
+        playerOneLifeVal += 5
+        updateLifeLabels()
+    }
+    
+    @IBAction func playerOneMinusOneTapped(_ sender: UIButton) {
+        playerOneLifeVal -= 1
+        updateLifeLabels()
+    }
+    
+    @IBAction func playerOneMinusFiveTapped(_ sender: UIButton) {
+        playerOneLifeVal -= 5
+        updateLifeLabels()
+    }
+    
+    @IBAction func playerTwoPlusTapped(_ sender: UIButton) {
+        playerTwoLifeVal += 1
+        updateLifeLabels()
+    }
+    
+    @IBAction func playerTwoPlusFiveTapped(_ sender: UIButton) {
+        playerTwoLifeVal += 5
+        updateLifeLabels()
+    }
+    
+    @IBAction func playerTwoMinusTapped(_ sender: UIButton) {
+        playerTwoLifeVal -= 1
+        updateLifeLabels()
+    }
+    
+    @IBAction func playerTwoMinusFiveTapped(_ sender: UIButton) {
+        playerTwoLifeVal -= 5
+        updateLifeLabels()
+    }
+    
+    func updateLifeLabels() {
+        if playerOneLifeVal <= 0 {
+            losingLabel.text = "Player One LOSES!"
+            losingLabel.isHidden = false
+            playerOneLifeVal = 20
+            playerTwoLifeVal = 20
+        }
+        if playerTwoLifeVal <= 0 {
+            losingLabel.text = "Player Two LOSES!"
+            losingLabel.isHidden = false
+            playerOneLifeVal = 20
+            playerTwoLifeVal = 20
+        }
+        playerOneLifeLabel.text = "\(playerOneLifeVal)"
+        playerTwoLifeLabel.text = "\(playerTwoLifeVal)"
+    }
+    
+   
 }
 

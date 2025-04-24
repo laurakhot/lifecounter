dc62a2251d4e6d1e0be4de5a93c3868cedc2015c
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-04-23 21:47:46 -0700

    implemented home screen of game

---
+++ lifecounter/HistoryViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HistoryViewController.swift
+//  lifecounter
+//
+//  Created by Laura Khotemlyansky on 4/23/25.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+    var history : [String] = []
+    
+   
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textLabel.text = history.joined(separator: "\n")
+    }
+    
+    
+    @IBAction func backPressed(_ sender: Any) {
+        performSegue(withIdentifier: "toHome", sender: self)
+    }
+
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -8,79 +8,106 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var gameStarted = false
+    var history : [String] = []
+    var numPlayers: Int = 4
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         losingLabel.isHidden = true
+        for i in 4..<players.count {
+            players[i].isHidden = true
+        }
+
     }
+    @IBOutlet var lifeLabels: [UILabel]!
     
-    var playerOneLifeVal = 20
-    var playerTwoLifeVal = 20
-    
-    @IBOutlet weak var playerOneLifeLabel: UILabel!
-    
-    @IBOutlet weak var playerTwoLifeLabel: UILabel!
-    
+    @IBOutlet var inputFields: [UITextField]!
+
+    @IBOutlet weak var addPlayer: UIButton!
+
     @IBOutlet weak var losingLabel: UILabel!
+    
+    @IBOutlet var players: [UIView]!
+    // implement code to add more players to the ui
+    @IBAction func addPlayer(_ sender: UIButton) {
+        if !gameStarted && numPlayers < players.count {
+            players[numPlayers].isHidden = false
+            numPlayers += 1
+            if numPlayers == players.count {
+                addPlayer.isEnabled = false
+            }
+        }
+    }
+    
+    @IBAction func addLife(_ sender: UIButton) {
+        checkButton()
+        let index = sender.tag - 1
+        var currAmount = Int(lifeLabels[index].text ?? "") ?? 0
+        let inputAmount = Int(inputFields[index].text ?? "") ?? 0
+        currAmount += inputAmount
+        inputFields[index].text = ""
+        lifeLabels[index].text = "\(currAmount)"
+        history.append("Player \(index + 1) gained \(inputAmount) life")
+        checkLoss(for: index)
+    }
+    
+    @IBAction func subtractLife(_ sender: UIButton) {
+        checkButton()
+        let index = sender.tag - 1
+        print(index)
+        var currAmount = Int(lifeLabels[index].text ?? "") ?? 0
+        let inputAmount = Int(inputFields[index].text ?? "") ?? 0
+        currAmount -= inputAmount
+        inputFields[index].text = ""
+        lifeLabels[index].text = "\(currAmount)"
+        history.append("Player \(index + 1) lost \(inputAmount) life")
+        checkLoss(for: index)
+    }
+    
+    
+    func checkButton() {
+        if !gameStarted {
+            gameStarted = true
+            addPlayer.isEnabled = false
+        }
+    }
 
-    @IBAction func playerOnePlusTapped(_ sender: UIButton) {
-        playerOneLifeVal += 1
-        updateLifeLabels()
-    }
     
-    @IBAction func playerOnePlusFiveTapped(_ sender: UIButton) {
-        playerOneLifeVal += 5
-        updateLifeLabels()
-    }
-    
-    @IBAction func playerOneMinusOneTapped(_ sender: UIButton) {
-        playerOneLifeVal -= 1
-        updateLifeLabels()
-    }
-    
-    @IBAction func playerOneMinusFiveTapped(_ sender: UIButton) {
-        playerOneLifeVal -= 5
-        updateLifeLabels()
-    }
-    
-    @IBAction func playerTwoPlusTapped(_ sender: UIButton) {
-        playerTwoLifeVal += 1
-        updateLifeLabels()
-    }
-    
-    @IBAction func playerTwoPlusFiveTapped(_ sender: UIButton) {
-        playerTwoLifeVal += 5
-        updateLifeLabels()
-    }
-    
-    @IBAction func playerTwoMinusTapped(_ sender: UIButton) {
-        playerTwoLifeVal -= 1
-        updateLifeLabels()
-    }
-    
-    @IBAction func playerTwoMinusFiveTapped(_ sender: UIButton) {
-        playerTwoLifeVal -= 5
-        updateLifeLabels()
-    }
-    
-    func updateLifeLabels() {
-        if playerOneLifeVal <= 0 {
-            losingLabel.text = "Player One LOSES!"
+    func checkLoss(for index: Int) {
+        if let life = Int(lifeLabels[index].text ?? ""), life <= 0 {
+            print(life)
+            losingLabel.text="Player \(index + 1) lost!"
             losingLabel.isHidden = false
-            playerOneLifeVal = 20
-            playerTwoLifeVal = 20
+            print(losingLabel.isHidden)
+            history.append("Player \(index + 1) lost!")
+            resetGame()
         }
-        if playerTwoLifeVal <= 0 {
-            losingLabel.text = "Player Two LOSES!"
-            losingLabel.isHidden = false
-            playerOneLifeVal = 20
-            playerTwoLifeVal = 20
+    }
+
+    func resetGame() {
+        gameStarted = false
+        numPlayers = 4
+        history = []
+        for i in 0..<lifeLabels.count {
+            lifeLabels[i].text = "20"
         }
-        playerOneLifeLabel.text = "\(playerOneLifeVal)"
-        playerTwoLifeLabel.text = "\(playerTwoLifeVal)"
+        addPlayer.isEnabled = true
+        for i in 4..<players.count {
+            players[i].isHidden = true
+        }
     }
     
-   
+    @IBAction func historyPressed(_ UIButton: Any) {
+        performSegue(withIdentifier: "toHistory", sender: self)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toHistory" {
+            let destinationVC = segue.destination as? HistoryViewController
+            destinationVC?.history = history
+        }
+    }
+    
 }
-
